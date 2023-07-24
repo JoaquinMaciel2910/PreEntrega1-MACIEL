59d1f8e46c92d358ab998c00a2c80ed2c9e243f9
JoaquinMaciel2910 <joaquinmaciel2910@gmail.com>
2023-07-23 23:42:03 -0300

    agregue los arrays, clase y metodos para la segunda pre-entrega

--- script.js
+++ script.js
@@ -1,44 +1,74 @@
-function jugarPiedraPapelTijera() {
-    const rondas = parseInt(prompt("ELEGI CUANTAS RONDAS JUGAR"));
-
-    let nombre1 = prompt("Indica tu nombre");
-    let nombre2 = prompt("Indica tu nombre");
+class Opcion {
+    constructor(nombre, venceA) {
+      this.nombre = nombre;
+      this.venceA = venceA;
+    }
+  
+    vence(opcion) {
+      return this.venceA.includes(opcion.nombre);
+    }
+  }
+  
+  function jugarPiedraPapelTijera() {
+    const rondas = parseInt(prompt("¿Cuántas rondas queres jugar?"));
+  
+    const opciones = [];
+  
+    const piedra = new Opcion("piedra", ["tijera"]);
+    const papel = new Opcion("papel", ["piedra"]);
+    const tijera = new Opcion("tijera", ["papel"]);
+  
+    opciones.push(piedra);
+    opciones.push(papel);
+    opciones.push(tijera);
+  
+    const nombreJugador1 = prompt("Ingresar el nombre del Jugador 1:");
+    const nombreJugador2 = prompt("Ingresar el nombre del Jugador 2:");
+  
     let victoriasJugador1 = 0;
     let victoriasJugador2 = 0;
-
+  
     for (let i = 1; i <= rondas; i++) {
-        console.log("Ronda " + i + ":");
-
-        const jugador1 = prompt(nombre1 + ", elegí : piedra, papel o tijera").toLowerCase();
-        const jugador2 = prompt(nombre2 + ", elegí : piedra, papel o tijera").toLowerCase();
-
-        console.log(nombre1 + ": " + jugador1);
-        console.log(nombre2 + ": " + jugador2);
-
-        if (jugador1 === jugador2) {
-            console.log("Empataron en la ronda " + i + ". Los dos eligieron " + jugador1 + "y" + jugador2 + ".");
-        } else if (
-            (jugador1 === "piedra" && jugador2 === "tijera") ||
-            (jugador1 === "papel" && jugador2 === "piedra") ||
-            (jugador1 === "tijera" && jugador2 === "papel")
-        ) {
-            console.log("¡Ganó " + nombre1 + " en la ronda " + i + "! ");
-            victoriasJugador1++;
-        } else {
-            console.log("¡Ganó " + nombre2 + " en la ronda " + i + "! ");
-            victoriasJugador2++;
-        }
-
-        console.log("---------------------------------------");
+      console.log("Ronda " + i + ":");
+  
+      const jugador1 = obtenerOpcionJugador(nombreJugador1, opciones);
+      const jugador2 = obtenerOpcionJugador(nombreJugador2, opciones);
+  
+      console.log(nombreJugador1 + ": " + jugador1.nombre);
+      console.log(nombreJugador2 + ": " + jugador2.nombre);
+  
+      if (jugador1.nombre === jugador2.nombre) {
+        console.log("Empate en la ronda " + i + ". Los dos eligieron " + jugador1.nombre + ".");
+      } else if (jugador1.vence(jugador2)) {
+        console.log("¡Ganó " + nombreJugador1 + " en la ronda " + i + "! " + jugador1.nombre + " le ganaste a " + jugador2.nombre + ".");
+        victoriasJugador1++;
+      } else {
+        console.log("¡Ganó " + nombreJugador2 + " en la ronda " + i + "! " + jugador2.nombre + " le ganaste a " + jugador1.nombre + ".");
+        victoriasJugador2++;
+      }
+  
+      console.log("---------------------------------------");
     }
-
+  
     if (victoriasJugador1 > victoriasJugador2) {
-        console.log("¡GANASTE " + "¡Ganó " + nombre1 + "! ");
+      console.log("¡Felicidades, " + nombreJugador1 + "! Ganaste el juego.");
     } else if (victoriasJugador1 < victoriasJugador2) {
-        console.log("¡GANASTE " + "¡Ganó " + nombre2 + "! ");
+      console.log("¡Felicidades, " + nombreJugador2 + "! Ganaste el juego.");
     } else {
-        console.log("¡EMPATARON! :(");
+      console.log("Empataron");
     }
-}
-
-jugarPiedraPapelTijera();
+  }
+  
+  function obtenerOpcionJugador(jugador, opciones) {
+    let opcion;
+    do {
+      opcion = prompt(jugador + ", elige piedra, papel o tijera").toLowerCase();
+    } while (!validarOpcion(opcion, opciones));
+    return opciones.find((o) => o.nombre === opcion);
+  }
+  
+  function validarOpcion(opcion, opcionesValidas) {
+    return opcionesValidas.some((o) => o.nombre === opcion);
+  }
+  
+  jugarPiedraPapelTijera();
