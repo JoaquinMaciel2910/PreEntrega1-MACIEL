776a5433b01534104bc3694dfca67f3c878c51c1
JoaquinMaciel2910 <joaquinmaciel2910@gmail.com>
2023-08-07 12:57:06 -0300

    personalizando css y html

--- script.js
+++ script.js
@@ -1,74 +1,68 @@
-class Opcion {
-    constructor(nombre, venceA) {
-      this.nombre = nombre;
-      this.venceA = venceA;
-    }
-  
-    vence(opcion) {
-      return this.venceA.includes(opcion.nombre);
-    }
+class Game {
+  constructor() {
+      this.choices = ['piedra', 'papel', 'tijera'];
+      this.result = document.getElementById('result');
+      this.playerNameInput = document.getElementById('playerName');
+      this.restoreGame();
   }
-  
-  function jugarPiedraPapelTijera() {
-    const rondas = parseInt(prompt("¿Cuántas rondas queres jugar?"));
-  
-    const opciones = [];
-  
-    const piedra = new Opcion("piedra", ["tijera"]);
-    const papel = new Opcion("papel", ["piedra"]);
-    const tijera = new Opcion("tijera", ["papel"]);
-  
-    opciones.push(piedra);
-    opciones.push(papel);
-    opciones.push(tijera);
-  
-    const nombreJugador1 = prompt("Ingresar el nombre del Jugador 1:");
-    const nombreJugador2 = prompt("Ingresar el nombre del Jugador 2:");
-  
-    let victoriasJugador1 = 0;
-    let victoriasJugador2 = 0;
-  
-    for (let i = 1; i <= rondas; i++) {
-      console.log("Ronda " + i + ":");
-  
-      const jugador1 = obtenerOpcionJugador(nombreJugador1, opciones);
-      const jugador2 = obtenerOpcionJugador(nombreJugador2, opciones);
-  
-      console.log(nombreJugador1 + ": " + jugador1.nombre);
-      console.log(nombreJugador2 + ": " + jugador2.nombre);
-  
-      if (jugador1.nombre === jugador2.nombre) {
-        console.log("Empate en la ronda " + i + ". Los dos eligieron " + jugador1.nombre + ".");
-      } else if (jugador1.vence(jugador2)) {
-        console.log("¡Ganó " + nombreJugador1 + " en la ronda " + i + "! " + jugador1.nombre + " le ganaste a " + jugador2.nombre + ".");
-        victoriasJugador1++;
-      } else {
-        console.log("¡Ganó " + nombreJugador2 + " en la ronda " + i + "! " + jugador2.nombre + " le ganaste a " + jugador1.nombre + ".");
-        victoriasJugador2++;
+
+  play(playerChoice) {
+      const playerName = this.playerNameInput.value.trim();
+      if (playerName === '') {
+          alert('Por favor, ingresa tu nombre antes de jugar.');
+          return;
       }
-  
-      console.log("---------------------------------------");
-    }
-  
-    if (victoriasJugador1 > victoriasJugador2) {
-      console.log("¡Felicidades, " + nombreJugador1 + "! Ganaste el juego.");
-    } else if (victoriasJugador1 < victoriasJugador2) {
-      console.log("¡Felicidades, " + nombreJugador2 + "! Ganaste el juego.");
-    } else {
-      console.log("Empataron");
-    }
+
+      const computerChoice = this.getRandomChoice();
+      const result = this.calculateResult(playerChoice, computerChoice);
+      this.showResult(playerName, playerChoice, computerChoice, result);
+      this.saveGame(playerName, playerChoice, computerChoice, result);
   }
-  
-  function obtenerOpcionJugador(jugador, opciones) {
-    let opcion;
-    do {
-      opcion = prompt(jugador + ", elige piedra, papel o tijera").toLowerCase();
-    } while (!validarOpcion(opcion, opciones));
-    return opciones.find((o) => o.nombre === opcion);
+
+  getRandomChoice() {
+      const randomIndex = Math.floor(Math.random() * this.choices.length);
+      return this.choices[randomIndex];
   }
-  
-  function validarOpcion(opcion, opcionesValidas) {
-    return opcionesValidas.some((o) => o.nombre === opcion);
+
+  calculateResult(playerChoice, computerChoice) {
+      if (playerChoice === computerChoice) {
+          return 'Empate';
+      } else if (
+          (playerChoice === 'piedra' && computerChoice === 'tijera') ||
+          (playerChoice === 'papel' && computerChoice === 'piedra') ||
+          (playerChoice === 'tijera' && computerChoice === 'papel')
+      ) {
+          return 'Ganaste';
+      } else {
+          return 'Perdiste';
+      }
   }
-  
-  jugarPiedraPapelTijera();
+
+  showResult(playerName, playerChoice, computerChoice, result) {
+      this.result.textContent = `${playerName}, elegiste: ${playerChoice}, Computadora eligió: ${computerChoice}. Resultado: ${result}`;
+  }
+
+  saveGame(playerName, playerChoice, computerChoice, result) {
+      const gameData = {
+          playerName,
+          playerChoice,
+          computerChoice,
+          result
+      };
+      localStorage.setItem('gameData', JSON.stringify(gameData));
+  }
+
+  restoreGame() {
+      const gameData = JSON.parse(localStorage.getItem('gameData'));
+      if (gameData) {
+          this.playerNameInput.value = gameData.playerName;
+          this.showResult(gameData.playerName, gameData.playerChoice, gameData.computerChoice, gameData.result);
+      }
+  }
+}
+
+const game = new Game();
+
+function play(playerChoice) {
+  game.play(playerChoice);
+}
